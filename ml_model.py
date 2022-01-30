453ae796d1198aabfbbc5c4ce2cf7f603266af63
Anubhav0209 <mishra1anubhav@gmail.com>
2022-01-30 10:30:34 +0530

    added 3 new ml model code

--- ml_model.py
+++ ml_model.py
@@ -1,2 +1,16 @@
 
 print("adding ML related code")
+
+
+def model1():
+    print("adding code for ML model1")
+
+
+
+def model2():
+    print("adding code for ML model2")
+
+
+
+def model3():
+    print("adding code for ML model3")
